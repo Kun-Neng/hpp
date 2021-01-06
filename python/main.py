d2598575a137cc1814b5b40cfb2b3e016700f314
Kun-Neng <kunneng.hung@gmail.com>
2021-01-06 09:13:03 +0800

    add dimension and waypoint data of 2D in the testing json

--- python/main.py
+++ python/main.py
@@ -3,7 +3,8 @@ from model import Model
 from a_star import AStar
 
 scenario = {
-    "dimension": {"x": 10, "y": 10, "z": 10},
+    "dimension": {"x": 15, "y": 15, "z": 0},  # 2D
+    # "dimension": {"x": 10, "y": 10, "z": 10},  # 3D
     # "data": {},
     # "data": {"size": 0, "x": [], "y": [], "z": []},
     "data": {
@@ -12,11 +13,16 @@ scenario = {
         "y": [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
         "z": [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
     },
-    "waypoint": {
-        "start": {"x": 5, "y": 9, "z": 2},
-        "stop": {"x": 5, "y": 0, "z": 4},
+    "waypoint": {  # 2D
+        "start": {"x": 12, "y": 0, "z": 0},
+        "stop": {"x": 1, "y": 11, "z": 0},
         "allowDiagonal": False
     },
+    # "waypoint": {  # 3D
+    #     "start": {"x": 5, "y": 9, "z": 2},
+    #     "stop": {"x": 5, "y": 0, "z": 4},
+    #     "allowDiagonal": False
+    # },
     "boundary": {
         "zCeil": 6,
         "zFloor": 1
@@ -26,8 +32,9 @@ scenario = {
 if __name__ == "__main__":
     model = Model(scenario)
     init_Q = model.create_initial_Q()
-    # print(len(init_Q))
     # print(init_Q)
+    # print(len(init_Q))
+    # print(len(init_Q["initQ"]))
 
     obstacle_array = create_obstacle_array(scenario["data"])
     a_star = AStar(init_Q, scenario, obstacle_array)
