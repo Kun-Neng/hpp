934e8bcd8596ecf6027519bffc04f14fed87e1a4
Kun-Neng <kunneng.hung@gmail.com>
2021-07-24 10:39:38 +0800

    TypeScript: add isCollinear and refinePathFromCollinearity functions in the Tools object

--- typescript/src/Tools.ts
+++ typescript/src/Tools.ts
@@ -1,5 +1,31 @@
 import {Grid} from './Grid';
 
+interface Point {
+    x: number;
+    y: number;
+    z?: number;
+}
+
+function isCollinear(p1: Point, p2: Point, p3: Point) {
+    const is2d = p1.z ? false : true;
+
+    if (is2d) {
+        return ((p3.y - p2.y) * (p2.x - p1.x) === (p2.y - p1.y) * (p3.x - p2.x));
+    } else {
+        const x21 = p2.x - p1.x;
+        const x31 = p3.x - p1.x;
+        const y21 = p2.y - p1.y;
+        const y31 = p3.y - p1.y;
+        const z21 = p2.z! - p1.z!;
+        const z31 = p3.z! - p1.z!;
+        const crossX = y21 * z31 - y31 * z21;
+        const crossY = x31 * z21 - x21 * z31;
+        const crossZ = x21 * y31 - x31 * y21;
+
+        return (crossX === 0) && (crossY === 0) && (crossZ === 0);
+    }
+}
+
 export default {
     findTheMinimum: (hashmap: Map<string, Grid>, crux: string) => {
         let key = '';
@@ -41,5 +67,105 @@ export default {
             y: newYArray.reverse(),
             z: newZArray.reverse()
         };
+    },
+    isCollinear,
+    refinePathFromCollinearity: (path: { x: number[], y: number[], z?: number[] }, debugMode = false) => {
+        const is2d = (!path.z || path.z!.length === 0) ? true : false;
+        const length = path.x.length;
+
+        if (length === 0) {
+            console.error(`[Error] Length of x array is zero.`);
+            return;
+        }
+
+        if (is2d) {
+            if (path.y.length !== length) {
+                console.error(`[Error] Inconsistent vector lengths of x and y arrays in the path.`);
+                return;
+            }
+        } else {
+            if (path.y.length !== length || path.z!.length !== length) {
+                console.error(`[Error] Inconsistent vector lengths of x and y/z arrays in the path.`);
+                return;
+            }
+        }
+        
+        const x: number[] = [];
+        const y: number[] = [];
+        const z: number[] = [];
+
+        let log = '';
+        for (let iPoint = 0; iPoint < length - 2; iPoint++) {
+            const tempStartPoint = { x: path.x[iPoint], y: path.y[iPoint], z: is2d ? undefined : path.z![iPoint] };
+            
+            log += `[Add] Distinct point at ${iPoint}: ${tempStartPoint.x}, ${tempStartPoint.y}`;
+            log += is2d ? '\n' : `, ${tempStartPoint.z}\n`;
+
+            x.push(tempStartPoint.x);
+            y.push(tempStartPoint.y);
+            if (tempStartPoint.z) {
+                z.push(tempStartPoint.z);
+            }
+
+            const nextPoint = { x: path.x[iPoint + 1], y: path.y[iPoint + 1], z: is2d ? undefined : path.z![iPoint + 1] };
+            
+            let isFinished = false;
+            for (let jPoint = iPoint + 2; jPoint < length; jPoint++) {
+                const futurePoint = { x: path.x[jPoint], y: path.y[jPoint], z: is2d ? undefined : path.z![jPoint] };
+                
+                if (isCollinear(tempStartPoint, nextPoint, futurePoint)) {
+                    log += `[Status] Collinear from ${iPoint} to ${jPoint}\n`;
+
+                    if (jPoint < length - 1) {
+                        continue;
+                    } else {
+                        const lastPoint = { x: path.x[jPoint], y: path.y[jPoint], z: is2d ? undefined : path.z![jPoint] };
+                        
+                        log += `[Status] The last ${jPoint - iPoint + 1} points are collinear.\n`;
+                        log += `[Add] Distinct point at ${jPoint}: ${lastPoint.x}, ${lastPoint.y}`;
+                        log += is2d ? '\n' : `, ${lastPoint.z}\n`;
+
+                        x.push(lastPoint.x);
+                        y.push(lastPoint.y);
+                        if (lastPoint.z) {
+                            z.push(lastPoint.z);
+                        }
+
+                        isFinished = true;
+                        break;
+                    }
+                } else {
+                    iPoint = jPoint - 2;
+                    log += `[Status] Update iPoint to ${iPoint}\n`;
+                    break;
+                }
+            }
+
+            if (isFinished) {
+                break;
+            }
+
+            if (iPoint === length - 3) {
+                const nextToLastPoint = { x: path.x[length - 2], y: path.y[length - 2], z: is2d ? undefined : path.z![length - 2] };
+                const lastPoint = { x: path.x[length - 1], y: path.y[length - 1], z: is2d ? undefined : path.z![length - 1] };
+                
+                log += `[Add] Distinct point at ${length - 2}: ${nextToLastPoint.x}, ${nextToLastPoint.y}`;
+                log += is2d ? '\n' : `, ${nextToLastPoint.z}\n`;
+                log += `[Add] Distinct point at ${length - 1}: ${lastPoint.x}, ${lastPoint.y}`;
+                log += is2d ? '\n' : `, ${lastPoint.z}\n`;
+
+                x.push(nextToLastPoint.x, lastPoint.x);
+                y.push(nextToLastPoint.y, lastPoint.y);
+                if (nextToLastPoint.z && lastPoint.z) {
+                    z.push(nextToLastPoint.z, lastPoint.z);
+                }
+            }
+        }
+
+        if (debugMode) {
+            console.log(log);
+        }
+
+        return { x, y, z };
     }
 }
--- typescript/src/__tests__/Tools.test.ts
+++ typescript/src/__tests__/Tools.test.ts
@@ -41,28 +41,6 @@ const scenario_2d = {
     }
 };
 
-const scenario_2d_no_results = {
-    "dimension": {"x": 15, "y": 15},
-    "waypoint": {
-        "start": {"x": 12, "y": 0},
-        "stop": {"x": 1, "y": 11},
-        "allowDiagonal": false
-    },
-    "data": {
-        "size": 28,
-        "x": [11, 11, 11, 12, 12, 13, 13, 13],
-        "y": [-1,  0,  1, -1,  1, -1,  0,  1]
-        // "x": [ 0,  1,  2,  2,
-        //        2,  2,  2,  2,  2,  2,  2,  2,
-        //        3,  4,  5,  6,  7,  8,  9, 10, 11, 12,
-        //       12, 12, 12, 12, 12, 12, 12, 12, 12, 12],
-        // "y": [ 5,  5, 13, 14,
-        //        5,  6,  7,  8,  9, 10, 11, 12,
-        //       12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
-        //        2,  3,  4,  5,  6,  7,  8,  9, 10, 11]
-    }
-};
-
 test('test_find_the_min_F', () => {
     const obstacle2DArray = Model.createObstacleArray(scenario_2d.data);
     const model2D = new Model(scenario_2d.dimension, obstacle2DArray, scenario_2d.waypoint);
@@ -103,4 +81,69 @@ test('test_create_path_from_finalQ', () => {
     const great_result2D = Tools.createPathFromFinalQ(hashmap2D, stopGrid);
     expect(great_result2D.x[0]).toBe(stopGrid.x);
     expect(great_result2D.y[0]).toBe(stopGrid.y);
+});
+
+test('test_is_collinear', () => {
+    const startPoint2D = {x: 5, y: 2};
+    const nextPoint2D = {x: 5, y: 4};
+    const futurePoint2D = {x: 5, y: 10};
+    expect(Tools.isCollinear(startPoint2D, nextPoint2D, futurePoint2D)).toBe(true);
+
+    const testPoint2D1 = {x: 8, y: 10};
+    expect(Tools.isCollinear(startPoint2D, nextPoint2D, testPoint2D1)).toBe(false);
+
+    const startPoint3D = {x: 2, y: 2, z: 3};
+    const nextPoint3D = {x: 2, y: 2, z: 6};
+    const futurePoint3D = {x: 2, y: 2, z: 8};
+    expect(Tools.isCollinear(startPoint3D, nextPoint3D, futurePoint3D)).toBe(true);
+
+    const testPoint3D1 = {x: 2, y: 4, z: 10};
+    expect(Tools.isCollinear(startPoint3D, nextPoint3D, testPoint3D1)).toBe(false);
+});
+
+test('test_refine_path_from_collinearity', () => {
+    const zeroLengthsPath = Tools.refinePathFromCollinearity({ x: [], y: [] });
+    expect(zeroLengthsPath).toBe(undefined);
+    const zeroZLengthPath = Tools.refinePathFromCollinearity({ x: [1, 2, 3], y: [1, 1, 1], z: [] });
+    expect(Number(zeroZLengthPath?.x.length)).toBe(2);
+    const inconsistentLengthPath2D = Tools.refinePathFromCollinearity({ x: [1, 2, 3], y: [1, 1, 1, 1] });
+    expect(inconsistentLengthPath2D).toBe(undefined);
+    const inconsistentLengthPath3D = Tools.refinePathFromCollinearity({ x: [1, 2, 3], y: [1, 1, 1], z: [2] });
+    expect(inconsistentLengthPath3D).toBe(undefined);
+
+    const nonDiagonalPath2DLastThreeCollinear = {
+        x: [12, 12, 11, 11, 11, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1],
+        y: [ 0,  1,  1,  2,  3,  4,  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5]
+    }; //    o   o   o           o                              o  o
+    const refinedNonDiagonalPath2DLastThreeCollinear = Tools.refinePathFromCollinearity(nonDiagonalPath2DLastThreeCollinear, true);
+    // console.log(refinedNonDiagonalPath2DLastThreeCollinear);
+    expect(Number(refinedNonDiagonalPath2DLastThreeCollinear?.x.length)).toBe(6);
+    
+    const nonDiagonalPath2D = {
+        x: [12, 12, 11, 11, 11, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1,  1,  1],
+        y: [ 0,  1,  1,  2,  3,  4,  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11]
+    }; //    o   o   o           o                              o                      o
+    const refinedNonDiagonalPath2D = Tools.refinePathFromCollinearity(nonDiagonalPath2D, true);
+    // console.log(refinedNonDiagonalPath2D);
+    expect(Number(refinedNonDiagonalPath2D?.x.length)).toBe(6);
+
+    const nonDiagonalPath = {
+        x: [5, 5, 5, 4, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5],
+        y: [9, 8, 7, 7, 7, 6, 5, 4, 3, 2, 2, 1, 1, 0],
+        z: [2, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4]
+    }; //   o     o     o     o     o  o  o  o  o  o
+    const refinedNonDiagonalPath = Tools.refinePathFromCollinearity(nonDiagonalPath, true);
+    // console.log(refinedNonDiagonalPath);
+    expect(Number(nonDiagonalPath.x.length)).toBe(14);
+    expect(Number(refinedNonDiagonalPath?.x.length)).toBe(10);
+
+    const diagonalPath = {
+        x: [5, 5, 4, 3, 3, 3, 4, 4, 5, 5, 5],
+        y: [9, 8, 7, 6, 5, 4, 3, 2, 3, 2, 1],
+        z: [2, 2, 2, 2, 2, 2, 3, 3, 4, 4, 4]
+    }; //   o  o     o     o  o  o  o     o
+    const refinedDiagonalPath = Tools.refinePathFromCollinearity(diagonalPath, true);
+    // console.log(refinedDiagonalPath);
+    expect(Number(diagonalPath.x.length)).toBe(11);
+    expect(Number(refinedDiagonalPath?.x.length)).toBe(8);
 });
