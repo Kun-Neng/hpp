a450b3e32c3bb9936802429d58e754965e74c2c7
Kun-Neng <kunneng.hung@gmail.com>
2021-10-29 16:23:41 +0800

    Bumped version number to 0.1.5

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyhpp",
-    version="0.1.4.2",
+    version="0.1.5",
     author="Kun-Neng Hung",
     author_email="kunneng.hung@gmail.com",
     description="Algorithms for Three Dimensional Path Planning",
